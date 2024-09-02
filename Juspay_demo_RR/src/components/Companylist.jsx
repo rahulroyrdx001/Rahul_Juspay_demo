4da3bad31644bba35dd0c8c2e95b471146bdfc56
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2024-09-02 13:58:12 +0530

    latest update

--- Juspay_demo_RR/src/components/Companylist.jsx
+++ Juspay_demo_RR/src/components/Companylist.jsx
@@ -5,47 +5,7 @@ export default function Companylist() {
     <div className={style.companymain}>
       <div classname={style.marquee_container}>
         <div className={style.marquee}>
-          <img
-            src="https://juspay.io/icons/partners/agoda_white_expand.svg"
-            alt="agoda"
-          />
-          <img
-            src="https://juspay.io/icons/partners/amazon_white.svg"
-            alt="amazon"
-          />
-          <img
-            src="https://juspay.io/icons/partners/burger_king_white_expand.svg"
-            alt="burgerking"
-          />
-          <img
-            src="https://juspay.io/icons/partners/flipkart_walmart_white_expand.svg"
-            alt="flipkart"
-          />
-          <img
-            src="https://juspay.io/icons/partners/google_white.svg"
-            alt="google"
-          />
-          <img
-            src="https://juspay.io/icons/partners/indigo_white_expand.svg"
-            alt="indigo"
-          />
-          <img
-            src="https://juspay.io/icons/partners/macdonalds_white_expand.svg"
-            alt="macdonalds"
-          />
-          <img
-            src="https://juspay.io/icons/partners/microsoft_white.svg"
-            alt="microsoft"
-          />
-          <img
-            src="https://juspay.io/icons/partners/one_plus.svg"
-            alt="oneplus"
-          />
           {/* <img
-            src="https://juspay.io/icons/partners/starbucks_white_expand.svg"
-            alt="starbuks"
-          /> */}
-          <img
             src="https://juspay.io/icons/partners/agoda_white_expand.svg"
             alt="agoda"
           />
@@ -81,6 +41,42 @@ export default function Companylist() {
             src="https://juspay.io/icons/partners/one_plus.svg"
             alt="oneplus"
           />
+          <img
+            src="https://juspay.io/icons/partners/agoda_white_expand.svg"
+            alt="agoda"
+          />
+          <img
+            src="https://juspay.io/icons/partners/amazon_white.svg"
+            alt="amazon"
+          />
+          <img
+            src="https://juspay.io/icons/partners/burger_king_white_expand.svg"
+            alt="burgerking"
+          />
+          <img
+            src="https://juspay.io/icons/partners/flipkart_walmart_white_expand.svg"
+            alt="flipkart"
+          />
+          <img
+            src="https://juspay.io/icons/partners/google_white.svg"
+            alt="google"
+          />
+          <img
+            src="https://juspay.io/icons/partners/indigo_white_expand.svg"
+            alt="indigo"
+          />
+          <img
+            src="https://juspay.io/icons/partners/macdonalds_white_expand.svg"
+            alt="macdonalds"
+          />
+          <img
+            src="https://juspay.io/icons/partners/microsoft_white.svg"
+            alt="microsoft"
+          />
+          <img
+            src="https://juspay.io/icons/partners/one_plus.svg"
+            alt="oneplus"
+          /> */}
         </div>
       </div>
       <div className={style.videoelement}>
